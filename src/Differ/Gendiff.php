2432e75a06e8dc0e6a07f596a1a4957137d85c8c
mbaray <baraymasha@gmail.com>
2022-06-02 11:23:45 +0300

    Revert "modi"
    This reverts commit 8a2052dd788c54c68fd1954ffc35bc38357fd4da.

--- src/Differ/Gendiff.php
+++ src/Differ/Gendiff.php
@@ -13,32 +13,31 @@ function genDiff(string $pathToFile1, string $pathToFile2, string $formatName = 
     $firstArray = pathToArray($pathToFile1);
     $secondArray = pathToArray($pathToFile2);
 
-    $ast = makeAst($firstArray, $secondArray);
+    $replacedArray = array_replace_recursive($firstArray, $secondArray);
+    $sortReplacedArray = sortRecursive($replacedArray);
+
+    $ast = makeAst($sortReplacedArray, $firstArray, $secondArray);
 
     return formatterSelection($formatName, $ast);
 }
 
-function makeAst(array $arr1, array $arr2): array
+function makeAst(array $replacedArray, array $arr1, array $arr2): array
 {
-    $iter = function ($partArr1 = [], $partArr2 = [], $check = false) use (&$iter) {
-        if (!is_array($partArr2)) {
-            return $partArr2;
+    $iter = function ($currentValue, $check = false, $partArr1 = [], $partArr2 = []) use (&$iter) {
+        if (!is_array($currentValue)) {
+            return $currentValue;
         }
-
-        $current = array_replace($partArr1, $partArr2);
-        // ksort($currentValue);
-        $currentValue = mySort($current);
 
         return array_reduce(
             array_keys($currentValue),
-            function ($acc, $key) use ($iter, $check, $currentValue, $partArr1, $partArr2) {
+            function ($acc, $key) use (&$iter, $check, $currentValue, $partArr1, $partArr2) {
                 $culVal = $currentValue[$key];
 
                 if ($check === false) {
                     $child = [
                         "type" => makeType($culVal),
                         "key" => $key,
-                        "value" => $iter([], $culVal),
+                        "value" => $iter($culVal),
                     ];
 
                     return array_merge($acc, [$child]);
@@ -48,13 +47,13 @@ function makeAst(array $arr1, array $arr2): array
                 $inArr2 = is_array($partArr2) && array_key_exists($key, $partArr2);
 
                 if (!$inArr1) {
-                    $value = $iter([], $partArr2[$key]);
+                    $value = $iter($culVal);
                     $operation = "added";
                 } elseif (!$inArr2) {
-                    $value = $iter([], $partArr1[$key]);
+                    $value = $iter($culVal);
                     $operation = "deleted";
-                } elseif (is_array($partArr2[$key]) && is_array($partArr1[$key])) {
-                    $value = $iter($partArr1[$key], $partArr2[$key], true);
+                } elseif (is_array($culVal) && is_array($partArr1[$key])) {
+                    $value = $iter($culVal, true, $partArr1[$key], $partArr2[$key]);
                     $operation = "not_changed";
                 } elseif ($partArr1[$key] !== $partArr2[$key]) {
                     $child = [
@@ -62,8 +61,8 @@ function makeAst(array $arr1, array $arr2): array
                         "oldType" => makeType($partArr1[$key]),
                         "newType" => makeType($partArr2[$key]),
                         "key" => $key,
-                        "oldValue" => $iter([], $partArr1[$key]),
-                        "newValue" => $iter([], $partArr2[$key]),
+                        "oldValue" => $iter($partArr1[$key]),
+                        "newValue" => $iter($partArr2[$key]),
                     ];
 
                     return array_merge($acc, [$child]);
@@ -84,7 +83,7 @@ function makeAst(array $arr1, array $arr2): array
             []
         );
     };
-    $result = $iter($arr1, $arr2, true);
+    $result = $iter($replacedArray, true, $arr1, $arr2);
 
     return $result;
 }
@@ -107,7 +106,7 @@ function pathToArray(string $path): array
     return parse($fileContent, $type);
 }
 
-function mySort(array $arr): array
+function sortRecursive(array $arr): array
 {
     $sortArr = sort(
         $arr,
@@ -115,6 +114,5 @@ function mySort(array $arr): array
         true
     );
 
-    return $sortArr;
-    // return array_map(fn($value) => is_array($value) ? sortRecursive($value) : $value, $sortArr);
+    return array_map(fn($value) => is_array($value) ? sortRecursive($value) : $value, $sortArr);
 }
